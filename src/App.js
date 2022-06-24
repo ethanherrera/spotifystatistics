1605206912ceea9103edeab9a42b4c18cb69a1d3
Ethan Herrera <ethanherrera@berkeley.edu>
2022-06-24 16:01:57 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,7 +1,49 @@
+import { useEffect } from "react";
 import "./App.css";
+import GetArtists from "./GetArtists";
+
+const CLIENT_ID = "fa60583b5fcf419693fa909755dfcd7a";
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL_AFTER_LOGIN =
+  "https://github.com/ethanherrera/spotifystatistics.git";
+const SCOPES = ["user-top-read"];
+const SCOPES_URL_PARAM = SCOPES.join("%20");
+
+function handleLogin() {
+  window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+}
+
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+  const stringAfterHashtag = hash.substring(1);
+  const paramsInUrl = stringAfterHashtag.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+    const [key, value] = currentValue.split("=");
+    accumulater[key] = value;
+    console.log(accumulater);
+    return accumulater;
+  }, {});
+  console.log(paramsSplitUp);
+  return paramsSplitUp;
+};
 
 function App() {
-  return <div className="App"></div>;
+  useEffect(() => {
+    if (window.location.hash) {
+      const { access_token, expires_in, token_type } =
+        getReturnedParamsFromSpotifyAuth(window.location.hash);
+      localStorage.clear();
+      console.log(access_token);
+      localStorage.setItem("accessToken", access_token);
+      localStorage.setItem("expiresIn", expires_in);
+      localStorage.setItem("tokenType", token_type);
+    }
+  });
+  return (
+    <div className="App">
+      <button onClick={handleLogin}>Login</button>
+      <GetArtists />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/GetArtists.jsx
@@ -0,0 +1,44 @@
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+
+const TOP_ARTISTS_ENDPOINT = "https://api.spotify.com/v1/me/top/artists";
+
+function GetArtists() {
+  const [token, setToken] = useState("");
+  const [data, setData] = useState({});
+  useEffect(() => {
+    if (localStorage.getItem("accessToken")) {
+      setToken(localStorage.getItem("accessToken"));
+    }
+  }, [token]);
+
+  const handleButton = () => {
+    axios
+      .get(TOP_ARTISTS_ENDPOINT, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((res) => {
+        setData(res.data);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  };
+  return (
+    <>
+      <button onClick={handleButton}>Get Artists</button>
+      {data?.items
+        ? data.items.map((item) => (
+            <>
+              <p>{item.name}</p>
+              <img src={item.images[0].url} />
+            </>
+          ))
+        : null}
+    </>
+  );
+}
+
+export default GetArtists;
