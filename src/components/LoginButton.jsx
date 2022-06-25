0169c3c9c04a07963c9c9e1b507fa8c8a411825d
Ethan Herrera <ethanherrera@berkeley.edu>
2022-06-24 18:37:20 -0700

    seperation of state and church

---
+++ src/components/LoginButton.jsx
@@ -0,0 +1,31 @@
+const CLIENT_ID = "fa60583b5fcf419693fa909755dfcd7a";
+const SPOTIFY_AUTHORIZE_ENDPOINT = "https://accounts.spotify.com/authorize";
+const REDIRECT_URL_AFTER_LOGIN =
+  "https://ethanherrera.github.io/spotifystatistics/loggedin";
+const SCOPES = ["user-top-read"];
+const SCOPES_URL_PARAM = SCOPES.join("%20");
+
+function handleLogin() {
+  window.location = `${SPOTIFY_AUTHORIZE_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URL_AFTER_LOGIN}&scope=${SCOPES_URL_PARAM}&response_type=token&show_dialog=true`;
+}
+
+const getReturnedParamsFromSpotifyAuth = (hash) => {
+  const stringAfterHashtag = hash.substring(1);
+  const paramsInUrl = stringAfterHashtag.split("&");
+  const paramsSplitUp = paramsInUrl.reduce((accumulater, currentValue) => {
+    const [key, value] = currentValue.split("=");
+    accumulater[key] = value;
+    console.log(accumulater);
+    return accumulater;
+  }, {});
+  console.log(paramsSplitUp);
+  return paramsSplitUp;
+};
+
+function LoginButton() {
+  return (
+    <>
+      <button onClick={handleLogin}>Login</button>
+    </>
+  );
+}
